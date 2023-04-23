ca35562ebfc97b2090b6deb4f298de7c4e65ae0b
Murphy Lomboli <murphylomboli@outlook.com>
2023-04-23 02:14:45 -0700

    Project Tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,7 +4,7 @@
 require 'net/ssh'
 
 ssh_key = File.read("#{ENV['HOME']}/.ssh/school")
-ssh_host = '100.25.38.184'
+ssh_host = '107.23.119.223'
 ssh_user = 'ubuntu'
 
 Net::SSH.start(ssh_host, ssh_user, keys: [ssh_key]) do |ssh|
